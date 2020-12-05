0a428bdade37d5d891253c5f1ea310140cf49ae0
rokcej <rokcej1997@gmail.com>
2020-12-05 15:43:12 +0100

    Added day 5

---
+++ 2020/day05/part1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	int max = 0;
+
+	std::string seat;
+	while (std::cin >> seat) {
+		int row = 0, col = 0;
+		for (int i = 0; i < 7; ++i) {
+			if (seat[6 - i] == 'B') row += 1 << i;
+		}
+		for (int i = 0; i < 3; ++i) {
+			if (seat[9 - i] == 'R')	col += 1 << i;
+		}
+		int id = row * 8 + col;
+		if (id > max) max = id;
+	}
+
+	std::cout << max << std::endl;
+
+	return 0;
+}
---
+++ 2020/day05/part2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	int seats[1024] = { 0 };
+
+	std::string seat;
+	while (std::cin >> seat) {
+		int row = 0, col = 0;
+		for (int i = 0; i < 7; ++i) {
+			if (seat[6 - i] == 'B')	row += 1 << i;
+		}
+		for (int i = 0; i < 3; ++i) {
+			if (seat[9 - i] == 'R')	col += 1 << i;
+		}
+		int id = row * 8 + col;
+		seats[id] = 1;
+	}
+
+	for (int i = 1; i < 1023; ++i) {
+		if (!seats[i] && seats[i-1] && seats[i+1]) {
+			std::cout << i << std::endl;
+			break;
+		}
+	}
+
+	return 0;
+}
