d1e8d8d4347cc7839aefd0794c1a739326662533
rokcej <rokcej1997@gmail.com>
2020-12-05 15:50:11 +0100

    Updated day 5

--- 2020/day05/part2.cpp
+++ 2020/day05/part2.cpp
@@ -8,10 +8,10 @@ int main() {
 	while (std::cin >> seat) {
 		int row = 0, col = 0;
 		for (int i = 0; i < 7; ++i) {
-			if (seat[6 - i] == 'B')	row += 1 << i;
+			if (seat[6 - i] == 'B')	row |= 1 << i;
 		}
 		for (int i = 0; i < 3; ++i) {
-			if (seat[9 - i] == 'R')	col += 1 << i;
+			if (seat[9 - i] == 'R')	col |= 1 << i;
 		}
 		int id = row * 8 + col;
 		seats[id] = 1;
