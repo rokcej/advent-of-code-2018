3fc736980858222a991e0a231c48a23534db46ff
rokcej <rokcej1997@gmail.com>
2020-12-14 02:29:46 +0100

    Updated day 13

--- 2020/day13/part1.cpp
+++ 2020/day13/part1.cpp
@@ -5,10 +5,12 @@
 int main() {
 	std::vector<int> ids;
 
+	// Read input
 	int time;
 	std::string line;
 	std::cin >> time >> line;
 
+	// Parse input
 	for (size_t pos = 0, next; pos < line.length(); pos = next + 1) {
 		next = line.find(',', pos);
 		if (next == std::string::npos)
@@ -17,13 +19,14 @@ int main() {
 			ids.push_back(std::stol(line.substr(pos, next - pos)));
 	}
 
+	// Calculate result
 	int result = ids[0];
 	int min = result;
-	for (auto id : ids) {
-		int r = id - (time % id);
+	for (int i = 1; i < ids.size(); ++i) {
+		int r = ids[i] - (time % ids[i]);
 		if (r < min) {
 			min = r;
-			result = id;
+			result = ids[i];
 		}
 	}
 
--- 2020/day13/part2.cpp
+++ 2020/day13/part2.cpp
@@ -3,28 +3,31 @@
 #include <vector>
 #include <cmath>
 #include <utility>
+#include <array>
 
+// Greatest common divisor
 long long gcd(long long a, long long b) {
-	long long temp;
 	while (b > 0LL) {
-		temp = a;
+		long long a_old = a;
 		a = b;
-		b = temp % b;
+		b = a_old % b;
 	}
 	return a;
 }
-
+// Least common multiple
 long long lcm(long long a, long long b) {
 	return std::abs(a * b) / gcd(a, b);
 }
 
 int main() {
-	std::vector<int> ids;
-	std::vector<int> offs;
+	std::vector<int> ids; // Shuttle bus IDs
+	std::vector<int> offs; // Delays
 
+	// Read input
 	std::string line;
 	std::cin >> line >> line;
 
+	// Parse input
 	int count = 0;
 	for (size_t pos = 0, next; pos < line.length(); pos = next + 1) {
 		next = line.find(',', pos);
@@ -37,31 +40,28 @@ int main() {
 		++count;
 	}
 
+	// Find LCM of IDs
 	long long N = 1LL;
-	for (auto n : ids)
-		N = lcm(N, n);
+	for (auto id : ids)
+		N = lcm(N, id);
 
-	std::vector<std::pair<long long, long long>> parts;
-
-	long long result = 0LL;
+	// Compute sum parts
+	std::vector<std::array<long long, 2>> parts;
 	for (int i = 1; i < ids.size(); ++i) {
 		long long coeff = N / ids[i];
 		int k = 0;
 		while ((coeff * k + offs[i]) % ids[i] != 0)
 			++k;
-		result += coeff * k;
-		parts.emplace_back(coeff * k, -coeff * (ids[i] - k));
+		parts.push_back({coeff * k, -coeff * (ids[i] - k)});
 	}
 
-	for (int i = 1; i < (1 << parts.size()); ++i) {
+	// Find smallest positive sum
+	long long result;
+	for (int perm = 0; perm < (1 << parts.size()); ++perm) { // Permutations
 		long long sum = 0LL;
-		for (int j = 0; j < parts.size(); ++j) {
-			if ((1 << j) & i)
-				sum += parts[j].second;
-			else
-				sum += parts[j].first;
-		}
-		if (sum >= 0LL && sum < result)
+		for (int i = 0; i < parts.size(); ++i)
+			sum += parts[i][(perm >> i) & 1]; // Magic
+		if (sum >= 0LL && sum < result || perm == 0)
 			result = sum;
 	}
 
