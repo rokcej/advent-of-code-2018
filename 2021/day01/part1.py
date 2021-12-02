7206ec2f8249ab576eead06d37b6924d363bd8b1
Rok Cej <rokcej1997@gmail.com>
2021-12-02 16:39:09 +0100

    Add one-liner solutions for day 1

--- 2021/day01/part1.py
+++ 2021/day01/part1.py
@@ -1,11 +1,12 @@
-with open("input", "r") as f:
-    lines = f.readlines()
+print([sum(map(lambda a,b:a>b,x[1:],x[:-1])) for x in [[int(l) for l in open("input").read().splitlines()]]][0])
 
-depths = [int(line.strip()) for line in lines]
-count = 0
-for i in range(1, len(depths)):
-    if depths[i] > depths[i-1]:
-        count += 1
+# with open("input", "r") as f:
+#     lines = f.readlines()
 
-print(count)
-    
+# depths = [int(line.strip()) for line in lines]
+# count = 0
+# for i in range(1, len(depths)):
+#     if depths[i] > depths[i-1]:
+#         count += 1
+
+# print(count)
--- 2021/day01/part2.py
+++ 2021/day01/part2.py
@@ -1,14 +1,15 @@
-with open("input", "r") as f:
-    lines = f.readlines()
+print([sum([sum(x[i-2:i+1])>sum(x[i-3:i]) for i in range(3,len(x))]) for x in [[int(l) for l in open("input").read().splitlines()]]][0])
 
-depths = [int(line.strip()) for line in lines]
-count = 0
-window = sum(depths[:3])
-for i in range(3, len(depths)):
-    new_window = window + depths[i] - depths[i-3]
-    if new_window > window:
-        count += 1
-    window = new_window
+# with open("input", "r") as f:
+#     lines = f.readlines()
 
-print(count)
-    
+# depths = [int(line.strip()) for line in lines]
+# count = 0
+# window = sum(depths[:3])
+# for i in range(3, len(depths)):
+#     new_window = window + depths[i] - depths[i-3]
+#     if new_window > window:
+#         count += 1
+#     window = new_window
+
+# print(count)
