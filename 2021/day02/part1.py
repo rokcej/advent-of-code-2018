12b556dcf8a3cf98ce2421866a941cea6aa324a7
rokcej <rokcej1997@gmail.com>
2021-12-02 18:54:29 +0100

    Add one-liner solutions for day 2

--- 2021/day02/part1.py
+++ 2021/day02/part1.py
@@ -1,18 +1,20 @@
-with open("input", "r") as f:
-	input = f.read()
+print([sum([t[0] for t in x])*sum([t[1] for t in x]) for x in [[({"forward":(v,0),"down":(0,v),"up":(0,-v)}[c]) for c,v in [(l.split()[0],int(l.split()[1])) for l in open("input").read().splitlines()]]]][0])
 
-horizontal = 0
-depth = 0
-for line in input.splitlines():
-	fields = line.split(" ")
-	dir = fields[0]
-	dist = int(fields[1])
+# with open("input", "r") as f:
+# 	input = f.read()
 
-	if dir == "forward":
-		horizontal += dist
-	elif dir == "down":
-		depth += dist
-	else:
-		depth -= dist
+# horizontal = 0
+# depth = 0
+# for line in input.splitlines():
+# 	fields = line.split(" ")
+# 	dir = fields[0]
+# 	dist = int(fields[1])
+
+# 	if dir == "forward":
+# 		horizontal += dist
+# 	elif dir == "down":
+# 		depth += dist
+# 	else:
+# 		depth -= dist
 	
-print(horizontal * depth)
+# print(horizontal * depth)
--- 2021/day02/part2.py
+++ 2021/day02/part2.py
@@ -1,20 +1,22 @@
-with open("input", "r") as f:
-	input = f.read()
+print([sum([t[0] for t in z])*sum([t[1] for t in z]) for z in [[(y[0],y[0]*y[1]) for y in [[(x[i][0],sum([a for _,a in x[:i+1]])) for i in range(len(x))] for x in [[({"forward":(v,0),"down":(0,v),"up":(0,-v)}[c]) for c,v in [(l.split()[0],int(l.split()[1])) for l in open("input").read().splitlines()]]]][0]]]][0])
 
-horizontal = 0
-depth = 0
-aim = 0
-for line in input.splitlines():
-	fields = line.split(" ")
-	dir = fields[0]
-	dist = int(fields[1])
+# with open("input", "r") as f:
+# 	input = f.read()
 
-	if dir == "forward":
-		horizontal += dist
-		depth += aim * dist
-	elif dir == "down":
-		aim += dist
-	else:
-		aim -= dist
+# horizontal = 0
+# depth = 0
+# aim = 0
+# for line in input.splitlines():
+# 	fields = line.split(" ")
+# 	dir = fields[0]
+# 	dist = int(fields[1])
+
+# 	if dir == "forward":
+# 		horizontal += dist
+# 		depth += aim * dist
+# 	elif dir == "down":
+# 		aim += dist
+# 	else:
+# 		aim -= dist
 	
-print(horizontal * depth)
+# print(horizontal * depth)
