b2b61b5ef6cbd8e7b7aabe66d3e4d305cfd110a9
rokcej <rokcej1997@gmail.com>
2021-12-06 16:17:39 +0100

    Optimize day 6

--- 2021/day06/part1.py
+++ 2021/day06/part1.py
@@ -1,19 +1,34 @@
-with open("input") as f:
-	initial = [int(x) for x in f.read().split(",")]
+import numpy as np
 
-sim = [0]
-f = [1]
-for day in range(80):
-	n = len(sim)
-	for i in range(n):
-		sim[i] -= 1
-		if sim[i] < 0:
-			sim[i] = 6
-			sim.append(8)
-	f.append(len(sim))
+initial = np.loadtxt("input", dtype=np.uint64, delimiter=",")
+initial_indices, initial_counts = np.unique(initial, return_counts=True)
+counts = np.zeros(9, dtype=np.uint64)
+counts[initial_indices] = initial_counts
 
-result = 0
-for x in initial:
-	result += f[-(x+1)]
+for day in range(256):
+	counts = np.roll(counts, -1)
+	counts[6] += counts[-1]
 
-print(result)
+print(np.sum(counts))
+
+
+# # Original brute-force solution
+# with open("input") as f:
+# 	initial = [int(x) for x in f.read().split(",")]
+
+# sim = [0]
+# f = [1]
+# for day in range(80):
+# 	n = len(sim)
+# 	for i in range(n):
+# 		sim[i] -= 1
+# 		if sim[i] < 0:
+# 			sim[i] = 6
+# 			sim.append(8)
+# 	f.append(len(sim))
+
+# result = 0
+# for x in initial:
+# 	result += f[-(x+1)]
+
+# print(result)
--- 2021/day06/part2.py
+++ 2021/day06/part2.py
@@ -1,25 +1,40 @@
-with open("input") as f:
-	initial = [int(x) for x in f.read().split(",")]
+import numpy as np
 
-sim = [0]
-f = [1]
-for day in range(128):
-	n = len(sim)
-	for i in range(n):
-		sim[i] -= 1
-		if sim[i] < 0:
-			sim[i] = 6
-			sim.append(8)
-	f.append(len(sim))
+initial = np.loadtxt("input", dtype=np.uint64, delimiter=",")
+initial_indices, initial_counts = np.unique(initial, return_counts=True)
+counts = np.zeros(9, dtype=np.uint64)
+counts[initial_indices] = initial_counts
 
-for day in range(128):
-	val = 0
-	for x in sim:
-		val += f[max(0, day+1-x)]
-	f.append(val)
+for day in range(256):
+	counts = np.roll(counts, -1)
+	counts[6] += counts[-1]
 
-result = 0
-for x in initial:
-	result += f[-(x+1)]
+print(np.sum(counts))
 
-print(result)
+
+# # Original brute-force solution
+# with open("input") as f:
+# 	initial = [int(x) for x in f.read().split(",")]
+
+# sim = [0]
+# f = [1]
+# for day in range(128):
+# 	n = len(sim)
+# 	for i in range(n):
+# 		sim[i] -= 1
+# 		if sim[i] < 0:
+# 			sim[i] = 6
+# 			sim.append(8)
+# 	f.append(len(sim))
+
+# for day in range(128):
+# 	val = 0
+# 	for x in sim:
+# 		val += f[max(0, day+1-x)]
+# 	f.append(val)
+
+# result = 0
+# for x in initial:
+# 	result += f[-(x+1)]
+
+# print(result)
