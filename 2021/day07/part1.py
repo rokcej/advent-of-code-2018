0b2a33b301b5a72ee68a44290e9fa6ee7e3cc808
rokcej <rokcej1997@gmail.com>
2021-12-07 18:36:03 +0100

    Optimize day 7 solutions

--- 2021/day07/part1.py
+++ 2021/day07/part1.py
@@ -1,15 +1,7 @@
-print([min([sum([abs(c-x) for c in cs]) for x in range(min(cs),max(cs)+1)]) for cs in [list(map(int, open("input").read().split(",")))]][0])
+import numpy as np
 
-# with open("input") as f:
-# 	crabs = list(map(int, f.read().split(",")))
-
-# align_min = sum(crabs)
-
-# for x in range(min(crabs), max(crabs)+1):
-# 	align = 0
-# 	for crab in crabs:
-# 		align += abs(crab - x)
-# 	if align < align_min:
-# 		align_min = align
-
-# print(align_min)
+with open("input") as f:
+	crabs = np.array([int(x) for x in f.read().split(",")], dtype=np.int)
+median = np.median(crabs).astype(np.int)
+result = sum([abs(crab - median) for crab in crabs])
+print(result)
--- 2021/day07/part2.py
+++ 2021/day07/part2.py
@@ -1,17 +1,8 @@
-print([min([sum([abs(c-x)*(abs(c-x)+1)//2 for c in cs]) for x in range(min(cs),max(cs)+1)]) for cs in [list(map(int, open("input").read().split(",")))]][0])
+import numpy as np
 
-# with open("input") as f:
-# 	crabs = list(map(int, f.read().split(",")))
-
-# def S(n): return n * (n+1) // 2
-
-# align_min = sum(map(S, crabs))
-
-# for x in range(min(crabs), max(crabs)+1):
-# 	align = 0
-# 	for crab in crabs:
-# 		align += S(abs(crab - x))
-# 	if align < align_min:
-# 		align_min = align
-
-# print(align_min)
+with open("input") as f:
+	crabs = np.array([int(x) for x in f.read().split(",")], dtype=np.int)
+average = int(np.average(crabs))
+S = lambda n: n * (n+1) // 2
+result = sum([S(abs(crab - average)) for crab in crabs])
+print(result)
