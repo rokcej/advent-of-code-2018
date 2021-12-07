7cf880da7a0c873acca84ce56735e84db4552f61
rokcej <rokcej1997@gmail.com>
2021-12-07 19:34:10 +0100

    Fix day 7 part 2

--- 2021/day07/part2.py
+++ 2021/day07/part2.py
@@ -1,8 +1,11 @@
 import numpy as np
+import math
 
 with open("input") as f:
 	crabs = np.array([int(x) for x in f.read().split(",")], dtype=np.int)
-average = int(np.average(crabs))
+average = np.average(crabs)
 S = lambda n: n * (n+1) // 2
-result = sum([S(abs(crab - average)) for crab in crabs])
-print(result)
+results = []
+for target in range(math.floor(average), math.ceil(average) + 1):
+	results.append(sum([S(abs(crab - target)) for crab in crabs]))
+print(min(results))
