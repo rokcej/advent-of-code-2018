2a95f777da943c1e0596697a20d2697b0532b66e
rokcej <rokcej1997@gmail.com>
2022-12-11 11:30:47 +0100

    Add 2022 day 11

---
+++ 2022/day11/part2.py
@@ -0,0 +1,37 @@
+def gcd(a, b): # Greatest common divisor
+	if b == 0:
+		return a
+	return gcd(b, a % b)
+
+def lcm(a, b): # Least common multiple
+	return a // gcd(a, b) * b
+
+with open("input") as f:
+	lines = f.read().splitlines()
+
+monkeys = [] # Ook ook!
+for i in range(0, len(lines), 7):
+	items = [int(x) for x in lines[i+1][18:].split(", ")]
+	op = lines[i+2][23:].split()
+	test = int(lines[i+3][21:])
+	succ = int(lines[i+4][29:])
+	fail = int(lines[i+5][30:])
+	monkeys.append([items, op, test, succ, fail])
+
+mod = 1
+for monkey in monkeys:
+	mod = lcm(mod, monkey[2])
+
+counts = { i: 0 for i in range(len(monkeys)) }
+for round in range(10000):
+	for i, [items, op, test, succ, fail] in enumerate(monkeys):
+		counts[i] += len(items)
+		while (len(items) > 0):
+			worry = items.pop() # Order doesn't matter
+			operand = worry if op[1] == "old" else int(op[1])
+			worry = worry + operand if op[0] == "+" else worry * operand
+			worry %= mod # Monkey business
+			monkeys[succ if worry % test == 0 else fail][0].append(worry)
+
+first, second = sorted(counts.values(), reverse=True)[:2]
+print(first * second)
