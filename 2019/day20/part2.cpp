fdd15fa3f510d2f3282de28181acd42368e9d4bd
rokcej <rokcej1997@gmail.com>
2022-12-28 23:56:51 +0100

    Add 2019 day 20

---
+++ 2019/day20/part2.cpp
@@ -0,0 +1,162 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <queue>
+#include <tuple>
+
+void copy(const std::vector<std::string>& src, std::vector<std::string>& dest) {
+	dest.resize(src.size());
+	for (int i = 0; i < src.size(); ++i) {
+		dest[i] = src[i];
+	}
+}
+
+void disp(const std::vector<std::string>& map) {
+	for (int i = 0; i < map.size(); ++i) {
+		std::cout << map[i] << std::endl;
+	}
+	std::cout << std::endl;
+}
+
+int main() {
+	std::vector<std::string> map;
+	for (std::string line; std::getline(std::cin, line);) {
+		map.push_back(line);
+	}
+
+	// Get outer rectangle bounds
+	int x0_out, y0_out;
+	for (int y = 0; y < map.size(); ++y) {
+		int x = map[y].find('#');
+		if (x != std::string::npos) {
+			x0_out = x;
+			y0_out = y;
+			break;
+		}
+	}
+	int w_out = map[y0_out].find(' ', x0_out + 1) - x0_out;
+	int h_out = 0;
+	while (map[y0_out + (++h_out)][x0_out] != ' '); // Not the cleanest approach
+
+	// Get inner rectangle bounds
+	int x0_in, y0_in;
+	for (int y = y0_out + 1; y < map.size(); ++y) {
+		int x = map[y].find(' ', x0_out + 1);
+		if (x != std::string::npos && x < x0_out + w_out - 1) {
+			x0_in = x;
+			y0_in = y;
+			break;
+		}
+	}
+	int w_in = map[y0_in].find('#', x0_in + 1) - x0_in;
+	int h_in = 0;
+	while (map[y0_in + (++h_in)][x0_in] != '#');
+
+
+	std::map<std::string, std::vector<std::pair<int, int>>> labels;
+
+	// Get vertical portal positions
+	int vertical[2][4] = {{x0_out, x0_out + w_out - 1, y0_out, y0_out + h_out - 1}, {x0_in, x0_in + w_in - 1, y0_in + h_in, y0_in - 1}};
+	for (auto [x_start, x_stop, y_up, y_down] : vertical) {
+		for (int x = x_start; x <= x_stop; ++x) {
+			if (map[y_up][x] == '.') {
+				std::string id{map[y_up-2][x], map[y_up-1][x]};
+				labels[id].emplace_back(x, y_up);
+			}
+			if (map[y_down][x] == '.') {
+				std::string id{map[y_down+1][x], map[y_down+2][x]};
+				labels[id].emplace_back(x, y_down);
+			}
+		}
+	}
+
+	// Get horizontal portal positions
+	int horizontal[2][4] = {{y0_out, y0_out + h_out - 1, x0_out, x0_out + w_out - 1}, {y0_in, y0_in + h_in - 1, x0_in + w_in, x0_in - 1}};
+	for (auto [y_start, y_stop, x_left, x_right] : horizontal) {
+		for (int y = y_start; y <= y_stop; ++y) {
+			if (map[y][x_left] == '.') {
+				std::string id{map[y][x_left-2], map[y][x_left-1]};
+				labels[id].emplace_back(x_left, y);
+			}
+			if (map[y][x_right] == '.') {
+				std::string id{map[y][x_right+1], map[y][x_right+2]};
+				labels[id].emplace_back(x_right, y);
+			}
+		}
+	}
+
+	// Connect portals
+	int x_start, y_start;
+	int x_goal, y_goal;
+	std::map<std::pair<int, int>, std::pair<int, int>> portals;
+	for (const auto& [id, coords] : labels) {
+		if (id == "AA") {
+			x_start = coords[0].first;
+			y_start = coords[0].second;
+		} else if (id == "ZZ") {
+			x_goal = coords[0].first;
+			y_goal = coords[0].second;
+		} else {
+			portals[coords[0]] = coords[1];
+			portals[coords[1]] = coords[0];
+		}
+	}
+
+	// BFS baby
+	std::vector<std::vector<std::string>> maps = {{}};
+	copy(map, maps[0]);
+	std::queue<std::tuple<int, int, int>> bfs;
+	bfs.emplace(x_start, y_start, 0);
+	int steps = 0;
+	while (!bfs.empty()) {
+		for (int count = bfs.size(); count > 0; --count) {
+			auto [x, y, level] = bfs.front();
+			bfs.pop();
+
+			if (x == x_goal && y == y_goal && level == 0) {
+				std::cout << steps << std::endl;
+				return 0;
+			}
+
+			// Teleport
+			auto it = portals.find(std::make_pair(x, y));
+			if (it != portals.end()) {
+				auto [x2, y2] = it->second;
+				int level2 = level;
+				if (x == x0_out || x == x0_out + w_out - 1 || y == y0_out || y == y0_out + h_out - 1) {
+					--level2;
+				} else {
+					++level2;
+				}
+
+				if (level2 < 0) {
+					continue;
+				} else if (level2 >= maps.size()) {
+					maps.emplace_back();
+					copy(map, maps[level2]);
+				}
+
+				if (maps[level2][y2][x2] == '.') {
+					maps[level2][y2][x2] == '@';
+					bfs.emplace(x2, y2, level2);
+					continue;
+				}
+			}
+
+			// Move
+			int neighbors[4][2] = {{x-1, y}, {x+1, y}, {x, y-1}, {x, y+1}};
+			for (auto [x2, y2] : neighbors) {
+				if (maps[level][y2][x2] == '.') {
+					maps[level][y2][x2] = '@';
+					bfs.emplace(x2, y2, level);
+				}
+			}
+		}
+		++steps;
+	}
+
+	std::cout << "No path found" << std::endl;
+	
+	return 0;
+}
