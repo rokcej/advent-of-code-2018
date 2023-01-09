6d43d2e78ed39ef93041ecc5f0efd76432c5869c
rokcej <rokcej1997@gmail.com>
2023-01-09 22:42:15 +0100

    Add 2017 day 1

---
+++ 2017/day01/part2.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+
+namespace Day01 {
+
+    public class Part2 {
+
+        public static void Main(string[] args) {
+            string captcha = File.ReadAllLines("input")[0];
+            int sum = 0;
+            for (int i = 0; i < captcha.Length; i++) {
+                char c = captcha[i];
+                char c2 = captcha[(i + captcha.Length / 2) % captcha.Length];
+                if (c == c2) {
+                    sum += c - '0';
+                }
+            }
+            Console.WriteLine(sum);
+        }
+
+    }
+
+}
