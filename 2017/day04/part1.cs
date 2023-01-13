3bd60fefc39cb99817dc03c1f6410bde3d8c8083
rokcej <rokcej1997@gmail.com>
2023-01-13 23:03:20 +0100

    Add 2017 day 4

---
+++ 2017/day04/part1.cs
@@ -0,0 +1,29 @@
+namespace Day04;
+
+class Part1 {
+
+	public static void Run() {
+		string[] lines = File.ReadAllLines("input");
+
+		int num_valid = 0;
+		foreach (string line in lines) {
+			if (IsValid(line)) {
+				++num_valid;
+			}
+		}
+		Console.WriteLine(num_valid);
+	}
+
+	public static bool IsValid(string passphrase) {
+        string[] words = passphrase.Split(' ');
+        HashSet<string> set = new HashSet<string>();
+        foreach (string word in words) {
+            if (set.Contains(word)) {
+				return false;
+            }
+            set.Add(word);
+        }
+		return true;
+    }
+
+}
---
+++ 2017/day04/part2.cs
@@ -0,0 +1,35 @@
+namespace Day04;
+
+class Part2 {
+
+    public static void Run() {
+        string[] lines = File.ReadAllLines("input");
+
+        int num_valid = 0;
+        foreach (string line in lines) {
+            if (IsValid(line)) {
+                ++num_valid;
+            }
+        }
+        Console.WriteLine(num_valid);
+    }
+
+    public static bool IsValid(string passphrase) {
+        List<string> canonical_words = passphrase.Split(' ').Select(Canonicalize).ToList();
+        HashSet<string> set = new HashSet<string>();
+        foreach (string word in canonical_words) {
+            if (set.Contains(word)) {
+                return false;
+            }
+            set.Add(word);
+        }
+        return true;
+    }
+
+    public static string Canonicalize(string word) {
+        char[] characters = word.ToCharArray();
+        Array.Sort(characters);
+        return new string(characters);
+    }
+
+}
