03c5b6b835c8be252314ff27a2995092aca1f237
rokcej <rokcej1997@gmail.com>
2023-12-11 20:27:57 +0100

    Add 2023 day 11

---
+++ 2023/day11/part1.py
@@ -0,0 +1,23 @@
+with open("input") as f:
+	lines = f.read().splitlines()
+
+# Parse input
+galaxies = []
+for y, line in enumerate(lines):
+	for x, char in enumerate(line):
+		if char == "#":
+			galaxies.append((x, y))
+
+# Expand universe
+cols, rows = (set(v) for v in zip(*galaxies))
+for i, (x, y) in enumerate(galaxies):
+	x_new = x + sum(col not in cols for col in range(x))
+	y_new = y + sum(row not in rows for row in range(y))
+	galaxies[i] = (x_new, y_new)
+
+# Calculate pairwise distances
+distance_sum = 0
+for i, (x0, y0) in enumerate(galaxies):
+	for j, (x1, y1) in enumerate(galaxies[i:]):
+		distance_sum += abs(y1 - y0) + abs(x1 - x0)
+print(distance_sum)
