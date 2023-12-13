f602e6d7fccb375e637dcddfe191f7ecc23f54d4
rokcej <rokcej1997@gmail.com>
2023-12-13 01:24:13 +0100

    Add 2023 day 12

---
+++ 2023/day12/part2.py
@@ -0,0 +1,42 @@
+import functools
+
+COPY_COUNT = 5
+
+def get_valid_starts(springs, size):
+	starts = []
+	for i in range(len(springs) - size + 1):
+		j = i + size - 1
+		if all(s in ["#", "?"] for s in springs[i:j+1]):
+			is_left_valid = i == 0 or springs[i-1] != "#"
+			is_right_valid = j == len(springs) - 1 or springs[j+1] != "#"
+			if is_left_valid and is_right_valid:
+				starts.append(i)
+	return starts
+
+@functools.cache
+def get_arrangement_count(springs, order):
+	if not order:
+		return int("#" not in springs)
+	if sum(order) + len(order) - 1 > len(springs):
+		return 0
+
+	arrangement_count = 0
+	size = order[0]
+	lookup_count = springs.index("#") + size + 1 if "#" in springs else len(springs)
+	starts = get_valid_starts(springs[:lookup_count], size)
+	for start in starts:
+		arrangement_count += get_arrangement_count(springs[start+size+1:], order[1:]) # Awooga
+	return arrangement_count
+
+with open("input") as f:
+	lines = f.read().splitlines()
+
+records = []
+for line in lines:
+	springs, order_str = line.split()
+	order = tuple(map(int, order_str.split(",")))
+	unfolded_springs = springs + ("?" + springs) * (COPY_COUNT - 1)
+	unfolded_order = order * COPY_COUNT
+	records.append((unfolded_springs, unfolded_order))
+
+print(sum(get_arrangement_count(springs, order) for springs, order in records))
