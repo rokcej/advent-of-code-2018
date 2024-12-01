2187d6bb8135a7ac00bc5cbef3b4b3e510472fb1
rokcej <rokcej1997@gmail.com>
2024-12-01 16:49:41 +0100

    Add 2024 day 1

---
+++ 2024/day01/part1.rs
@@ -0,0 +1,28 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn read_lines() -> Vec<String> {
+	let file = File::open("input").expect("Error opening input");
+	return BufReader::new(file).lines().map(|l| l.expect("Error reading line")).collect();
+}
+
+fn main() {
+	let lines = read_lines();
+
+	let mut left_list : Vec<i32> = Vec::new();
+	let mut right_list : Vec<i32> = Vec::new();
+	for line in lines {
+		let mut iter = line.split_whitespace();
+		left_list.push(iter.next().unwrap().parse().unwrap());
+		right_list.push(iter.next().unwrap().parse().unwrap());
+	}
+
+	left_list.sort();
+	right_list.sort();
+
+	let mut total_distance : i32 = 0;
+	for (left, right) in left_list.into_iter().zip(right_list) {
+		total_distance += (right - left).abs();
+	}
+	println!("{total_distance}");
+}
