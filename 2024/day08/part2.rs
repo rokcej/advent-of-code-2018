a0ea95a7c39a509d9493b688c6a263c5db2bfb8b
rokcej <rokcej1997@gmail.com>
2024-12-08 14:40:11 +0100

    Add 2024 day 8

---
+++ 2024/day08/part2.rs
@@ -0,0 +1,62 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn read_lines() -> Vec<String> {
+    let file = File::open("day08/input").expect("Error opening input");
+    return BufReader::new(file)
+        .lines()
+        .map(|l| l.expect("Error reading line"))
+        .collect();
+}
+
+fn main() {
+    let lines = read_lines();
+
+    let mut antennas: HashMap<char, Vec<(i32, i32)>> = HashMap::new();
+    let mut antinodes: Vec<Vec<bool>> = lines
+        .iter()
+        .map(|s| s.chars().map(|_| false).collect())
+        .collect();
+    let mut antinode_count: i32 = 0;
+
+    let height = antinodes.len() as i32;
+    let width = antinodes[0].len() as i32;
+
+    // Get antenna locations
+    for (y, line) in lines.iter().enumerate() {
+        for (x, character) in line.char_indices() {
+            if character == '.' {
+                continue;
+            }
+            antennas
+                .entry(character)
+                .or_insert(Vec::new())
+                .push((y as i32, x as i32));
+        }
+    }
+
+    // Find antinodes
+    for positions in antennas.values() {
+        for (i, &(y0, x0)) in positions.iter().enumerate() {
+            for &(y1, x1) in &positions[i + 1..] {
+                let mut spawn_antinodes = |(mut y, mut x), (dy, dx)| loop {
+                    if !antinodes[y as usize][x as usize] {
+                        antinodes[y as usize][x as usize] = true;
+                        antinode_count += 1;
+                    }
+                    (y, x) = (y + dy, x + dx);
+                    if !(y >= 0 && y < height && x >= 0 && x < width) {
+                        break;
+                    }
+                };
+
+                let (dy, dx) = (y1 - y0, x1 - x0);
+                spawn_antinodes((y0, x0), (-dy, -dx));
+                spawn_antinodes((y1, x1), (dy, dx));
+            }
+        }
+    }
+
+    println!("{antinode_count}");
+}
