2ce8244713b5f6014e2ec12cdd8f3962e79bb8ae
rokcej <rokcej1997@gmail.com>
2024-12-14 00:12:44 +0100

    Convert script indentation to spaces

--- aoc.py
+++ aoc.py
@@ -11,168 +11,168 @@ import textwrap
 
 # Solution code templates
 def get_template(language, year, day, part):
-	match language:
-		case "cpp":
-			return textwrap.dedent(f"""\
-				#include <iostream>
+    match language:
+        case "cpp":
+            return textwrap.dedent(f"""\
+                #include <iostream>
 
-				int main() {{
-					// TODO
+                int main() {{
+                    // TODO
 
-					return 0;
-				}}
-				""")
+                    return 0;
+                }}
+                """)
 
-		case "cs":
-			return textwrap.dedent(f"""\
-				namespace Day{day:02};
-					
-				class Part{part} {{
-					
-					public static void Run() {{
-						// TODO
-					}}
+        case "cs":
+            return textwrap.dedent(f"""\
+                namespace Day{day:02};
+                    
+                class Part{part} {{
+                    
+                    public static void Run() {{
+                        // TODO
+                    }}
 
-				}}
-				""")
-				
-		case "py":
-			return "# TODO\n"
-		
-		case "rs":
-			return textwrap.dedent(f"""\
-				fn main() {{
-					// TODO
-				}}
-				""")
+                }}
+                """)
+                
+        case "py":
+            return "# TODO\n"
+        
+        case "rs":
+            return textwrap.dedent(f"""\
+                fn main() {{
+                    // TODO
+                }}
+                """)
 
-	assert False, f"Template not found for language {language}"
+    assert False, f"Template not found for language {language}"
 
 # Commands for compiling and executing solutions
 def get_commands(language, year, day, part):
-	match language:
-		case "cpp":
-			return {
-				"dir":     f"./{year}/day{day:02}",
-				"compile": ["g++", "-std=c++17", "-O2", "-o", f"part{part}", f"part{part}.cpp"],
-				"execute": [f"./part{part}"]
-			}
-		case "cs":
-			return {
-				"dir":     f"./{year}",
-				"compile": ["dotnet", "build", "-o", "./bin/build", "--nologo", "-v", "q", "-clp:NoSummary"],
-				"execute": ["dotnet", f"./bin/build/{year}.dll", f"{day}", f"{part}"]
-			}
-		case "py":
-			return {
-				"dir":     f"./{year}/day{day:02}",
-				"compile": None,
-				"execute": ["python", f"part{part}.py"]
-			}
-		case "rs":
-			return {
-				"dir":     f"./{year}",
-				"compile": ["cargo", "build", "--release", "--bin", f"day{day:02}_part{part}"],
-				"execute": [f"./target/release/day{day:02}_part{part}"]
-			}
+    match language:
+        case "cpp":
+            return {
+                "dir":     f"./{year}/day{day:02}",
+                "compile": ["g++", "-std=c++17", "-O2", "-o", f"part{part}", f"part{part}.cpp"],
+                "execute": [f"./part{part}"]
+            }
+        case "cs":
+            return {
+                "dir":     f"./{year}",
+                "compile": ["dotnet", "build", "-o", "./bin/build", "--nologo", "-v", "q", "-clp:NoSummary"],
+                "execute": ["dotnet", f"./bin/build/{year}.dll", f"{day}", f"{part}"]
+            }
+        case "py":
+            return {
+                "dir":     f"./{year}/day{day:02}",
+                "compile": None,
+                "execute": ["python", f"part{part}.py"]
+            }
+        case "rs":
+            return {
+                "dir":     f"./{year}",
+                "compile": ["cargo", "build", "--release", "--bin", f"day{day:02}_part{part}"],
+                "execute": [f"./target/release/day{day:02}_part{part}"]
+            }
 
-	assert False, f"Commands not found for language {language}"
+    assert False, f"Commands not found for language {language}"
 
 
 # Setup solution template and input
 def setup(language, year, day, session):
-	dir_path = Path(f"{year}/day{day:02}")
+    dir_path = Path(f"{year}/day{day:02}")
 
-	# Create directory
-	print("Creating directory:     ", end="")
-	if dir_path.is_dir():
-		print("Already exists")
-	else:
-		dir_path.mkdir(parents=True)
-		print("Done")
+    # Create directory
+    print("Creating directory:     ", end="")
+    if dir_path.is_dir():
+        print("Already exists")
+    else:
+        dir_path.mkdir(parents=True)
+        print("Done")
 
-	# Create parts 1 and 2
-	for part in [1, 2]:
-		print(f"Creating part {part}:        ", end="")
-		file_path = dir_path / f"part{part}.{language}"
-		if file_path.is_file():
-			print("Already exists")
-		else:
-			with file_path.open("w") as f:
-				f.write(get_template(language, year, day, part))
-			print("Done")
+    # Create parts 1 and 2
+    for part in [1, 2]:
+        print(f"Creating part {part}:        ", end="")
+        file_path = dir_path / f"part{part}.{language}"
+        if file_path.is_file():
+            print("Already exists")
+        else:
+            with file_path.open("w") as f:
+                f.write(get_template(language, year, day, part))
+            print("Done")
 
-	# Download input
-	print("Downloading input:      ", end="")
-	input_path = dir_path / "input"
-	if input_path.is_file():
-		print("Already exists")
-	else:
-		response = requests.get(url=f"https://adventofcode.com/{year}/day/{day}/input", cookies={"session": session})
-		if response.status_code == 200:
-			with input_path.open("w") as f:
-				f.write(response.text)
-			print("Done")
-		else:
-			print(f"Error {response.status_code}: {response.reason}")
+    # Download input
+    print("Downloading input:      ", end="")
+    input_path = dir_path / "input"
+    if input_path.is_file():
+        print("Already exists")
+    else:
+        response = requests.get(url=f"https://adventofcode.com/{year}/day/{day}/input", cookies={"session": session})
+        if response.status_code == 200:
+            with input_path.open("w") as f:
+                f.write(response.text)
+            print("Done")
+        else:
+            print(f"Error {response.status_code}: {response.reason}")
 
 # Compile and execute solution
 def run(language, year, day, part):
-	print(f"================")
-	print(f"==== Part {part} ====")
-	print(f"================\n")
+    print(f"================")
+    print(f"==== Part {part} ====")
+    print(f"================\n")
 
-	commands = get_commands(language, year, day, part)
+    commands = get_commands(language, year, day, part)
 
-	# Compile
-	if commands["compile"] is not None:
-		try:
-			compile = subprocess.run(commands["compile"], cwd=commands["dir"])
-		except FileNotFoundError:
-			print(f"Error: Compiler '{commands['compile'][0]}' not found\n")
-			return
-		if compile.returncode != 0:
-			print(f"\nError: Unable to compile '{year}/day{day:02}/part{part}.{language}'\n")
-			return
+    # Compile
+    if commands["compile"] is not None:
+        try:
+            compile = subprocess.run(commands["compile"], cwd=commands["dir"])
+        except FileNotFoundError:
+            print(f"Error: Compiler '{commands['compile'][0]}' not found\n")
+            return
+        if compile.returncode != 0:
+            print(f"\nError: Unable to compile '{year}/day{day:02}/part{part}.{language}'\n")
+            return
 
-	# Execute
-	with open(f"{year}/day{day:02}/input", "r") as f:
-		time_start = time.time()
-		try:
-			execute = subprocess.run(commands["execute"], stdin=f.fileno(), cwd=commands["dir"], text=True)
-		except FileNotFoundError:
-			print(f"Error: Executable '{commands['execute'][0]}' not found\n")
-			return
-		duration = time.time() - time_start
-	print(f"\nTime: {duration:.3f}s\n")
-	if execute.returncode != 0:
-		print(f"Error: Exited with return code {execute.returncode}\n")
+    # Execute
+    with open(f"{year}/day{day:02}/input", "r") as f:
+        time_start = time.time()
+        try:
+            execute = subprocess.run(commands["execute"], stdin=f.fileno(), cwd=commands["dir"], text=True)
+        except FileNotFoundError:
+            print(f"Error: Executable '{commands['execute'][0]}' not found\n")
+            return
+        duration = time.time() - time_start
+    print(f"\nTime: {duration:.3f}s\n")
+    if execute.returncode != 0:
+        print(f"Error: Exited with return code {execute.returncode}\n")
 
 # Parse command line arguments
 def main():
-	parser = argparse.ArgumentParser(description="Advent of Code solution manager")
-	parser.add_argument("-l", required=True, type=str, choices=["cpp", "cs", "py", "rs"], help="select language")
-	parser.add_argument("-y", required=True, type=int, choices=range(2015, 2100), metavar="{2015,...}", help="select year")
-	parser.add_argument("-d", required=True, type=int, choices=range(1, 26), metavar="{1,...,25}", help="select day")
-	parser.add_argument("command", type=str, choices=["setup", "run"], help="setup directory or run solution(s)")
-	parser.add_argument("part", type=int, choices=[1, 2], nargs="?", help="(optional) choose solution part")
+    parser = argparse.ArgumentParser(description="Advent of Code solution manager")
+    parser.add_argument("-l", required=True, type=str, choices=["cpp", "cs", "py", "rs"], help="select language")
+    parser.add_argument("-y", required=True, type=int, choices=range(2015, 2100), metavar="{2015,...}", help="select year")
+    parser.add_argument("-d", required=True, type=int, choices=range(1, 26), metavar="{1,...,25}", help="select day")
+    parser.add_argument("command", type=str, choices=["setup", "run"], help="setup directory or run solution(s)")
+    parser.add_argument("part", type=int, choices=[1, 2], nargs="?", help="(optional) choose solution part")
 
-	args = parser.parse_args()
-	match args.command:
-		case "setup":
-			if not Path(".session").is_file():
-				print("Error: Please create a '.session' file containing your login session cookie")
-				return
-			with open(".session", "r") as f:
-				session = f.read().strip()
-			setup(args.l, args.y, args.d, session)
-		case "run":
-			if args.part is None:	
-				run(args.l, args.y, args.d, 1)
-				run(args.l, args.y, args.d, 2)
-			else:
-				run(args.l, args.y, args.d, args.part)
+    args = parser.parse_args()
+    match args.command:
+        case "setup":
+            if not Path(".session").is_file():
+                print("Error: Please create a '.session' file containing your login session cookie")
+                return
+            with open(".session", "r") as f:
+                session = f.read().strip()
+            setup(args.l, args.y, args.d, session)
+        case "run":
+            if args.part is None:	
+                run(args.l, args.y, args.d, 1)
+                run(args.l, args.y, args.d, 2)
+            else:
+                run(args.l, args.y, args.d, args.part)
 
 
 if __name__ == "__main__":
-	main()
+    main()
--- aoc.sh
+++ aoc.sh
@@ -18,120 +18,120 @@ session=""			# Login session cookie
 # Functions
 
 function help {
-	echo -e "Usage:"
-	echo -e "  $(basename $0) <options> <command>"
-	echo -e ""
-	echo -e "Options:"
-	echo -e "  -l [cpp, python]        Select language"
-	echo -e "  -y <2015..>             Select year"
-	echo -e "  -d <1..25>              Select day"
-	echo -e ""
-	echo -e "Commands:"
-	echo -e "  setup                   Setup directory and download input"
-	echo -e "  run [1, 2]              Compile and run solutions"
+    echo -e "Usage:"
+    echo -e "  $(basename $0) <options> <command>"
+    echo -e ""
+    echo -e "Options:"
+    echo -e "  -l [cpp, python]        Select language"
+    echo -e "  -y <2015..>             Select year"
+    echo -e "  -d <1..25>              Select day"
+    echo -e ""
+    echo -e "Commands:"
+    echo -e "  setup                   Setup directory and download input"
+    echo -e "  run [1, 2]              Compile and run solutions"
 }
 
 function run {
-	local language="$1"
-	local year="$2"
-	local day="$3"
-	local part="$4"
+    local language="$1"
+    local year="$2"
+    local day="$3"
+    local part="$4"
 
-	local dir="$(printf "$year/day%02d" $day)"
-	local file="part$part"
+    local dir="$(printf "$year/day%02d" $day)"
+    local file="part$part"
 
-	echo "================"
-	echo "==== Part $part ===="
-	echo "================"
-	echo ""
+    echo "================"
+    echo "==== Part $part ===="
+    echo "================"
+    echo ""
 
-	case "$language" in
-		cpp|c++)
-			if $(g++ -std=c++17 -O2 -o "$dir/$file" "$dir/$file.cpp"); then
-				(cd "$dir" ; time "./$file" < "input")
-			else
-				echo "Error compiling $dir/$file.cpp"
-			fi
-			;;
-		python|py)
-			(cd "$dir" ; time python3 "$file.py" < "input")
-			;;
-		*)
-			echo "Error: Unsupported language $language"
-			help
-			exit 1
-			;;
-	esac
+    case "$language" in
+        cpp|c++)
+            if $(g++ -std=c++17 -O2 -o "$dir/$file" "$dir/$file.cpp"); then
+                (cd "$dir" ; time "./$file" < "input")
+            else
+                echo "Error compiling $dir/$file.cpp"
+            fi
+            ;;
+        python|py)
+            (cd "$dir" ; time python3 "$file.py" < "input")
+            ;;
+        *)
+            echo "Error: Unsupported language $language"
+            help
+            exit 1
+            ;;
+    esac
 
-	echo ""
+    echo ""
 }
 
 function setup {
-	local language="$1"
-	local year="$2"
-	local day="$3"
-	local session="$4"
+    local language="$1"
+    local year="$2"
+    local day="$3"
+    local session="$4"
 
-	# Select file extension and content based on language
-	local extension=""
-	local content=""
-	case "$language" in
-		cpp|c++)
-			extension="cpp"
-			content="#include <iostream>\n\nint main() {\n\t\n\n\treturn 0;\n}\n"
-			;;
-		python|py)
-			extension="py"
-			content="\n"
-			;;
-		*)
-			echo "Error: Unsupported language $language"
-			help
-			exit 1
-			;;
-	esac
+    # Select file extension and content based on language
+    local extension=""
+    local content=""
+    case "$language" in
+        cpp|c++)
+            extension="cpp"
+            content="#include <iostream>\n\nint main() {\n\t\n\n\treturn 0;\n}\n"
+            ;;
+        python|py)
+            extension="py"
+            content="\n"
+            ;;
+        *)
+            echo "Error: Unsupported language $language"
+            help
+            exit 1
+            ;;
+    esac
 
-	# Create directory
-	local dir="$(printf "$year/day%02d" $day)"
-	echo -n -e "Creating directory: \t"
-	if [ ! -d "$dir" ]; then
-		if ! $(mkdir -p "$dir"); then
-			echo "Error: Unable to create $dir"
-			exit 1
-		fi
-		echo "Done"
-	else
-		echo "Already exists"
-	fi
+    # Create directory
+    local dir="$(printf "$year/day%02d" $day)"
+    echo -n -e "Creating directory: \t"
+    if [ ! -d "$dir" ]; then
+        if ! $(mkdir -p "$dir"); then
+            echo "Error: Unable to create $dir"
+            exit 1
+        fi
+        echo "Done"
+    else
+        echo "Already exists"
+    fi
 
-	# Create code file
-	for i in 1 2; do
-		echo -n -e "Creating part $i: \t"
-		if [ ! -f "$dir/part$i.$extension" ]; then
-			if ! $(touch "$dir/part$i.$extension"); then
-				echo "Error: Unable to create $dir/part$i.$extension"
-				exit 1
-			fi
-			echo -e -n "$content" >> "$dir/part$i.$extension"
-			echo "Done"
-		else
-			echo "Already exists"
-		fi
-	done
+    # Create code file
+    for i in 1 2; do
+        echo -n -e "Creating part $i: \t"
+        if [ ! -f "$dir/part$i.$extension" ]; then
+            if ! $(touch "$dir/part$i.$extension"); then
+                echo "Error: Unable to create $dir/part$i.$extension"
+                exit 1
+            fi
+            echo -e -n "$content" >> "$dir/part$i.$extension"
+            echo "Done"
+        else
+            echo "Already exists"
+        fi
+    done
 
-	# Download input
-	echo -n -e "Downloading input: \t"
-	if [ ! -f "$dir/input" ]; then
-		if ! $(curl https://adventofcode.com/$year/day/$day/input -b "session=$session" -o "$dir/input" -s); then
-			echo "Error: Unable to download $dir/input"
-			exit 1
-		fi
-		echo "Done"
-	else
-		echo "Already exists"
-	fi
+    # Download input
+    echo -n -e "Downloading input: \t"
+    if [ ! -f "$dir/input" ]; then
+        if ! $(curl https://adventofcode.com/$year/day/$day/input -b "session=$session" -o "$dir/input" -s); then
+            echo "Error: Unable to download $dir/input"
+            exit 1
+        fi
+        echo "Done"
+    else
+        echo "Already exists"
+    fi
 
-	echo "Setup for $year day $day complete"
+    echo "Setup for $year day $day complete"
 }
 
 
@@ -139,94 +139,94 @@ function setup {
 
 # Print usage
 if [ $# -lt 1 ]; then
-	help
-	exit 1
+    help
+    exit 1
 fi
 
 # Parse options
 while getopts ":l:y:d:" opt; do
-	case "$opt" in
-	l)
-		language="$OPTARG"
-		;;
-	y)
-		year="$OPTARG"
-		if ! ( [ "$year" -ge 2015 ] && [ "$year" -le 2099 ] ); then
-			echo "Invalid -$opt argument: Wrong year number $year" >&2
-			help
-			exit 1
-		fi
-		;;
-	d)
-		day="$OPTARG"
-		if ! ( [ "$day" -ge 1 ] && [ "$day" -le 25 ] ); then
-			echo "Invalid -$opt argument: Wrong day number $day" >&2
-			help
-			exit 1
-		fi
-		;;
-	\?)
-		echo "Invalid option: -$OPTARG" >&2
-		help
-		exit 1
-		;;
-	:)
-		echo "Missing argument: -$OPTARG requires an argument" >&2
-		help
-		exit 1
-		;;
-	esac
+    case "$opt" in
+    l)
+        language="$OPTARG"
+        ;;
+    y)
+        year="$OPTARG"
+        if ! ( [ "$year" -ge 2015 ] && [ "$year" -le 2099 ] ); then
+            echo "Invalid -$opt argument: Wrong year number $year" >&2
+            help
+            exit 1
+        fi
+        ;;
+    d)
+        day="$OPTARG"
+        if ! ( [ "$day" -ge 1 ] && [ "$day" -le 25 ] ); then
+            echo "Invalid -$opt argument: Wrong day number $day" >&2
+            help
+            exit 1
+        fi
+        ;;
+    \?)
+        echo "Invalid option: -$OPTARG" >&2
+        help
+        exit 1
+        ;;
+    :)
+        echo "Missing argument: -$OPTARG requires an argument" >&2
+        help
+        exit 1
+        ;;
+    esac
 done
 shift $((OPTIND -1))
 
 if [ -z "$language" ]; then
-	echo "Missing option: -l is required" >&2
-	help
-	exit 1
+    echo "Missing option: -l is required" >&2
+    help
+    exit 1
 fi
 if [ -z "$year" ]; then
-	echo "Missing option: -y is required" >&2
-	help
-	exit 1
+    echo "Missing option: -y is required" >&2
+    help
+    exit 1
 fi
 if [ -z "$day" ]; then
-	echo "Missing option: -d is required" >&2
-	help
-	exit 1
+    echo "Missing option: -d is required" >&2
+    help
+    exit 1
 fi
 
 # Parse commands
 subcommand="$1"
 shift
 case "$subcommand" in
-	setup)
-		# Read session cookie
-		if [ -z "$session"]; then
-			if [ ! -f "./.session" ]; then
-				echo "Error: Please create a .session file containing only your login session cookie (no newline characters)"
-				exit 1
-			fi
-			session="$(<./.session)"
-		fi
-		setup $language $year $day $session
-		;;
-	run)
-		if [ $# -lt 1 ]; then
-			run $language $year $day 1
-			run $language $year $day 2
-		else
-			part=$1
-			shift
-			if ! ( [ "$part" -eq 1 ] || [ "$part" -eq 2 ] ); then
-				echo "Invalid $subcommand argument: Wrong part number $part" >&2
-				exit 1
-			fi
-			run $language $year $day $part
-		fi
-		;;
-	*)
-		echo "Error: Invalid command"
-		help
-		exit 1
-		;;
+    setup)
+        # Read session cookie
+        if [ -z "$session"]; then
+            if [ ! -f "./.session" ]; then
+                echo "Error: Please create a .session file containing only your login session cookie (no newline characters)"
+                exit 1
+            fi
+            session="$(<./.session)"
+        fi
+        setup $language $year $day $session
+        ;;
+    run)
+        if [ $# -lt 1 ]; then
+            run $language $year $day 1
+            run $language $year $day 2
+        else
+            part=$1
+            shift
+            if ! ( [ "$part" -eq 1 ] || [ "$part" -eq 2 ] ); then
+                echo "Invalid $subcommand argument: Wrong part number $part" >&2
+                exit 1
+            fi
+            run $language $year $day $part
+        fi
+        ;;
+    *)
+        echo "Error: Invalid command"
+        help
+        exit 1
+        ;;
 esac
